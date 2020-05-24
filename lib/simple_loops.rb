2b243a5e15966f5a4f602f966bde2173b8662561
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-24 20:02:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+def loop_message_five_times(message)
+  counter = 0 
+  while counter < 6
+  puts message 
+  counter +=1 
+end
+
+def loop_message_n_times
+  
+end
+
+def output_array
+  
+end
+
+def return_string_away
+  
+end
+
