cfdc052e4074a8ab610585c2c3a20d185e92035f
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-24 20:24:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,7 +15,11 @@ def loop_message_n_times(message, n)
 end
 
 def output_array(array)
-  
+  counter = 0 
+  while counter < array.length do 
+    puts array[counter]
+    counter += 1 
+  end 
 end
 
 def return_string_away
